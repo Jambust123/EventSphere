dad52186158cd03843b599c599f89f17123e63e5
Jake Slattery <alexander.slattery@googlemail.com>
2025-01-14 14:56:57 +0000

    removed logs for debugging

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+	path: `${__dirname}/../.env.${ENV}`,
 });
-
-console.log(`Environment: ${ENV}`);
-console.log(`PGDATABASE: ${process.env.PGDATABASE}`);
-console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
 
 const config = {};
 
-if (ENV === 'production') {
-  config.connectionString = process.env.PGDATABASE || process.env.DATABASE_URL;
-  config.max = 2;
+if (ENV === "production") {
+	config.connectionString = process.env.PGDATABASE || process.env.DATABASE_URL;
+	config.max = 2;
 } else {
-  config.connectionString = process.env.PGDATABASE;
+	config.connectionString = process.env.PGDATABASE;
 }
 
 if (!config.connectionString) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+	throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
