cfa08f0d5140aa9cc880540d757c80a1a5c4aeaa
Jake Slattery <alexander.slattery@googlemail.com>
2025-01-16 14:42:06 +0000

    added db setup to listen file

--- listen.js
+++ listen.js
@@ -1,4 +1,17 @@
 const app = require("./app");
+const { exec } = require("child_process");
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+exec("npm run setup-dbs", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error running setup script: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Setup script stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Setup script stdout: ${stdout}`);
+
+  app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+});
