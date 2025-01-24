9417c8e86f00504531e2190f4b159a5af1f58218
Jake Slattery <alexander.slattery@googlemail.com>
2025-01-24 19:01:11 +0000

    added token.json handling

--- calendarAuth/googleAuth.js
+++ calendarAuth/googleAuth.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 const { google } = require("googleapis");
-const readline = require("readline");
 const path = require("path");
 
 const TOKEN_PATH = path.join(__dirname, "token.json");
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];
 
-async function authorize() {
+async function authorize(code) {
     const client_id = process.env.GOOGLE_CLIENT_ID;
     const client_secret = process.env.GOOGLE_CLIENT_SECRET;
     const redirect_uris = [process.env.GOOGLE_REDIRECT_URI];
@@ -17,34 +16,26 @@ async function authorize() {
         redirect_uris[0]
     );
 
-    if (fs.existsSync(TOKEN_PATH)) {
-        const token = fs.readFileSync(TOKEN_PATH);
-        oAuth2Client.setCredentials(JSON.parse(token));
+    if (code) {
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+        console.log("Token stored to", TOKEN_PATH);
         return oAuth2Client;
     }
 
-    return getAccessToken(oAuth2Client);
-}
+    const tokenJson = process.env.GOOGLE_TOKEN_JSON;
+    if (tokenJson) {
+        const token = JSON.parse(tokenJson);
+        oAuth2Client.setCredentials(token);
+        return oAuth2Client;
+    }
 
-function getAccessToken(oAuth2Client) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: "offline",
         scope: SCOPES,
     });
     console.log("Authorize this app by visiting this url:", authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question("Enter the code from that page here: ", (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error("Error retrieving access token", err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
-            console.log("Token stored to", TOKEN_PATH);
-        });
-    });
 }
 
 module.exports = authorize;
