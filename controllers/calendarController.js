88f956609f4837d53d4b5b0740c6e415675c0381
Jake Slattery <alexander.slattery@googlemail.com>
2025-01-24 19:06:54 +0000

    changed a function name

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -1,6 +1,34 @@
 const { google } = require("googleapis");
 const authorize = require("../calendarAuth/googleAuth");
 
+async function listEvents(req, res) {
+  try {
+    const auth = await authorize();
+    const calendar = google.calendar({ version: 'v3', auth });
+    calendar.events.list(
+      {
+        calendarId: 'primary',
+        timeMin: new Date().toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+      },
+      (err, result) => {
+        if (err) return res.status(500).send('There was an error contacting the Calendar service: ' + err);
+        const events = result.data.items;
+        if (events.length) {
+          res.status(200).json(events);
+        } else {
+          res.status(200).send('No upcoming events found.');
+        }
+      }
+    );
+  } catch (error) {
+    console.error("Error listing events:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+}
+
 async function createEvent(req, res) {
   try {
     const auth = await authorize();
