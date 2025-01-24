52e12ba67a2cd1c3ddd05ad807bb2c7473088eec
Jake Slattery <alexander.slattery@googlemail.com>
2025-01-24 16:52:00 +0000

    added cors for development

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const calendarController = require('../controllers/calendarController');
+const { authenticate } = require('../middleware/authentication');
+const cors = require('cors');
 
 const router = express.Router();
 
-router.get('/events', calendarController.listEvents);
-router.post('/events', calendarController.createEvent);
+router.options('*', cors()); 
+
+router.get('/events', authenticate, calendarController.listEvents);
+router.post('/events', authenticate, calendarController.createEvent);
 
 module.exports = router;
