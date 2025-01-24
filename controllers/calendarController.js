dba541928e2749c85888f72e6215362ce131ae91
Jake Slattery <alexander.slattery@googlemail.com>
2025-01-24 20:50:13 +0000

    calendar controller fix

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -1,3 +1,4 @@
+// filepath: /Ubuntu/home/jambust123/gradProject/BE/controllers/calendarController.js
 const { google } = require("googleapis");
 const authorize = require("../calendarAuth/googleAuth");
 
@@ -19,4 +20,32 @@ async function listEvents(req, res) {
   }
 }
 
-module.exports = { listEvents };
+async function createEvent(req, res) {
+  try {
+    const auth = await authorize();
+    const calendar = google.calendar({ version: "v3", auth });
+    const event = {
+      summary: req.body.summary,
+      location: req.body.location,
+      description: req.body.description,
+      start: {
+        dateTime: req.body.start,
+        timeZone: 'America/Los_Angeles',
+      },
+      end: {
+        dateTime: req.body.end,
+        timeZone: 'America/Los_Angeles',
+      },
+    };
+    const response = await calendar.events.insert({
+      calendarId: 'primary',
+      resource: event,
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error creating event:", error);
+    res.status(500).send("Error creating event");
+  }
+}
+
+module.exports = { listEvents, createEvent };
